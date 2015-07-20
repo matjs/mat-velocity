c358f3d896b447788fe346f06b5f2d439383b971
weikang.zhuwk <weikang.zhuwk@alibaba-inc.com>
2015-07-20 17:42:33 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # mat-velocity
+
+[![npm version](https://badge.fury.io/js/mat-velocity.svg)](http://badge.fury.io/js/mat-velocity)
+
 提供velocity模板的渲染功能
+
+
+## Installation
+
+```sh
+npm install --save mat-velocity
+```
+
+## Usage
+
+```javascript
+var mat  = require('mat')
+var velocity = require('mat-velocity')
+var rewrite = require('mat-rewrite')
+
+mat.task('daily', function () {
+  mat.url([/\.vm/])
+    .use(velocity({
+    }))
+})
+```
+
+## api
+
+用来处理vm的渲染。
+默认情况下 请求 b.vm 会使用 b.js返回的变量来渲染vm。
+可以使用b.vm?ds=c.js来指定不同的数据源
+
+* config.viewLocation  vm模板的目录，默认是当前根目录。
+* config.dataLocation  vm模板对应的渲染变量文件的目录，默认是当前根目录。
+* config.macros        全局的宏定义。在vm中使用  #xxx()  来使用。
+* config.dataInject    用于对返回的变量进行统一处理。这样可以注入一些全局统一的变量或者方法，支持返回promise对象处理异步。return的值如果不为空，就会整个替换（同理promise的情况下，就是resolve的值不为空）。
---
+++ index.js
@@ -0,0 +1,113 @@
+var _ = require('lodash')
+var fs = require('fs')
+var Stream = require('stream')
+var path = require('path')
+var velocity = require('velocityjs');
+
+var resolve = path.resolve
+var join = path.join
+
+
+var __CONFIG = {
+  viewLocation: './',
+  dataLocation: './',
+  defaultLayout: 'layout/default_layout.vm',
+  macros:{
+    parse: function (file) {
+      var viewPath = path.join(__CONFIG.viewLocation, file)
+      var template = fs.readFileSync(viewPath).toString()
+      return this.eval(template)
+    }
+  },
+  dataInject:function(data){
+    //需要被重写
+  }
+}
+
+var LAYOUT_REG = /set[\s]*\([\s]*\$layout[\s]*=[\s]*"([^"]*)"\)[\s]*/
+
+
+var fetchTmpl = function(viewPath) {
+  viewPath = resolve(__CONFIG.viewLocation,'./'+viewPath)
+  return fs.readFileSync(viewPath).toString()
+}
+
+var fetchData = function(dataSource) {
+
+  if (!dataSource) return
+
+  dataSource = resolve(__CONFIG.dataLocation,'./'+dataSource)
+  function fetch(callback) {
+
+    var filePath = dataSource
+
+    if (!fs.existsSync(filePath)) {
+      callback(null,{})
+    }
+
+    try {
+      callback(null,require(filePath))
+    } catch (e) {
+      callback(e)
+    }
+  }
+
+  return fetch
+}
+
+var parseLayout = function(template){
+
+  var layoutPath = __CONFIG.defaultLayout
+  var m = template.match(LAYOUT_REG)
+
+  if (m) {
+    layoutPath = m[1]
+  }
+  if (layoutPath) {
+    template = fetchTmpl(layoutPath).replace('$screen_content',template)
+  }
+
+  return template
+}
+
+function cobody(stream) {
+  return function(cb){
+    var buffers = []
+    stream.on('data', function(chunk){
+      buffers.push(chunk)
+    })
+    stream.on('end', function(){
+      cb(null, Buffer.concat(buffers).toString('utf-8'))
+    })
+  }
+}
+
+module.exports = function(config){
+
+  _.extend(__CONFIG,config)
+
+  return function *(next){
+    //this.path = join(__CONFIG.viewLocation,this.path)
+
+    //yield next
+
+    var ctx = this
+    var data = yield fetchData((this.query.ds || ctx.path.split('.')[0]+'.js'))
+    var template = fetchTmpl(this.path)
+
+    //对于stream进行转换
+    // if (template instanceof Stream) {
+    //   template = yield cobody(template)
+    // }else{
+    //   template = template.toString()
+    // }
+    //解析layout，默认的vm里面是没有layout功能的
+    template = parseLayout(template)
+
+    //提供统一的数据注入方法
+    __CONFIG.dataInject(data)
+
+    ctx.body = velocity.render(template, data, __CONFIG.macros)
+    ctx.type = 'text/html'
+  }
+}
